eead55b8c862f2e33125af9c71b71dbb4713bec7
MADConsultoria <comercial@madconsultoria.com.br>
2026-09-21 12:25:15 -0300

    Initial Portes Engenharia website

---
+++ _server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+
+const MIME = {
+  '.html': 'text/html; charset=utf-8',
+  '.js': 'text/javascript; charset=utf-8',
+  '.css': 'text/css',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon',
+};
+
+const root = __dirname;
+
+const server = http.createServer((req, res) => {
+  let reqPath = decodeURIComponent(url.parse(req.url).pathname);
+  if (reqPath === '/') reqPath = '/Portes Engenharia.dc.html';
+  const filePath = path.join(root, reqPath);
+  if (!filePath.startsWith(root)) {
+    res.writeHead(403);
+    return res.end('Forbidden');
+  }
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      return res.end('Not found: ' + reqPath);
+    }
+    const ext = path.extname(filePath).toLowerCase();
+    res.writeHead(200, { 'Content-Type': MIME[ext] || 'application/octet-stream' });
+    res.end(data);
+  });
+});
+
+server.listen(8000, () => {
+  console.log('Server running at http://localhost:8000/');
+});
